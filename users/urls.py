219b8b60be826703d54c6124baaa8254fde6d775
Dmitry <exigo53@gmail.com>
2024-11-21 23:24:59 +0300

    email

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import RegisterView
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'), name='users/login'),
-    path('logout/', LogoutView.as_view(next_page='http://127.0.0.1:8000/'), name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html', redirect_authenticated_user=True, next_page='/'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
